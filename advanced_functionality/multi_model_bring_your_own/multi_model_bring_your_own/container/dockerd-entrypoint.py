bbc69be183c95384b05cda93842197860f52115f
Harish Wajjala <hwajjala@roblox.com>
2023-10-07 10:50:48 -0700

    update container logic

--- advanced_functionality/multi_model_bring_your_own/multi_model_bring_your_own/container/dockerd-entrypoint.py
+++ advanced_functionality/multi_model_bring_your_own/multi_model_bring_your_own/container/dockerd-entrypoint.py
@@ -2,6 +2,7 @@ import os
 import shlex
 import subprocess
 import sys
+import clip
 from subprocess import CalledProcessError
 
 from retrying import retry
@@ -12,22 +13,23 @@ def _retry_if_error(exception):
     return isinstance(exception, CalledProcessError or OSError)
 
 
-@retry(stop_max_delay=1000 * 50, retry_on_exception=_retry_if_error)
+# @retry(stop_max_delay=1000 * 50, retry_on_exception=_retry_if_error)
 def _start_mms():
     # by default the number of workers per model is 1, but we can configure it through the
     # environment variable below if desired.
     # os.environ['SAGEMAKER_MODEL_SERVER_WORKERS'] = '2'
+    print("Starting the inference server workers.")
     model_server.start_model_server(handler_service="/home/model-server/model_handler.py:handle")
 
 
-def main():
-    if sys.argv[1] == "serve":
-        _start_mms()
-    else:
-        subprocess.check_call(shlex.split(" ".join(sys.argv[1:])))
+# def main():
+#     if sys.argv[1] == "serve":
+#         _start_mms()
+#     else:
+#         subprocess.check_call(shlex.split(" ".join(sys.argv[1:])))
 
-    # prevent docker exit
-    subprocess.call(["tail", "-f", "/dev/null"])
+#     # prevent docker exit
+#     subprocess.call(["tail", "-f", "/dev/null"])
 
 
-main()
+_start_mms()
