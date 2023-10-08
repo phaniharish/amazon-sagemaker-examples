2403f05dafc624289c45e08d6ecbdb89a792d3d3
Harish Wajjala <hwajjala@roblox.com>
2023-10-07 23:33:35 -0700

    Use pytorch containers

--- advanced_functionality/pytorch_extend_container_train_deploy_bertopic/container/bert-topic/bert-topic-inference.py
+++ advanced_functionality/pytorch_extend_container_train_deploy_bertopic/container/bert-topic/bert-topic-inference.py
@@ -16,7 +16,12 @@ import logging
 import os
 import torch
 import json
-from bertopic import BERTopic
+from PIL import Image
+import requests
+import clip
+import numpy as np
+import io
+
 
 JSON_CONTENT_TYPE = 'application/json'
 
@@ -28,12 +33,25 @@ def model_fn(model_dir):
     logger.info(f"inside model_fn, model_dir= {model_dir}")
     device = "cuda" if torch.cuda.is_available() else "cpu"
     logger.info("Device Type: {}".format(device))
-    model = BERTopic.load(os.path.join(model_dir, 'my_model'))
-    return model
+    clip_model, clip_preprocessor = clip.load("ViT-B/32", device="cpu")
+    clip_model.requires_grad_(False)
+    logger.info("Model loaded with Clip")
+    return clip_model, clip_preprocessor
+
 
 def predict_fn(data, model):
     logger.info(f'Got input Data: {data}')
-    return model.transform(data)
+    thumbnail_url = data.get("thumbnail_url")
+    logger.info(f"thumbnail_url: {thumbnail_url}")
+    parsed_image = Image.open(requests.get(thumbnail_url, stream=True).raw)
+    logger.info(f"parsed_image: {parsed_image}")
+    img_pre = model[1](parsed_image).unsqueeze(0)
+    logger.info(f"img_pre: {img_pre}")
+    logger.info(f"img_pre.shape: {img_pre.shape}")
+    with torch.no_grad():
+        img_features = model[0].encode_image(img_pre).cpu().numpy()
+    return img_features
+
 
 def input_fn(serialized_input_data, content_type=JSON_CONTENT_TYPE):
     logger.info(f"serialized_input_data object: {serialized_input_data}")
@@ -41,22 +59,14 @@ def input_fn(serialized_input_data, content_type=JSON_CONTENT_TYPE):
         input_data = json.loads(serialized_input_data)
         logger.info(f"input_data object: {input_data}")
         return input_data
-
     else:
         raise Exception('Requested unsupported ContentType in Accept: ' + content_type)
         return
-    
+
+
 def output_fn(prediction, content_type):
     logger.info(f"prediction object before: {prediction}, type: {type(prediction)}")
-    prediction = list(prediction)
-    logger.info(f"prediction object after: {prediction}, type: {type(prediction)}")
-    prediction[0] = [int(res_class) for res_class in prediction[0]]
-    logger.info(f"prediction[0] object after: {prediction[0]}, type: {type(prediction[0])}")
-    prediction[1] = [float(res_class) for res_class in prediction[1]]
-    logger.info(f"prediction[1] object after: {prediction[0]}, type: {type(prediction[1])}")    
-    prediction_result = { "predictions": prediction[0],
-                         "scores": prediction[1],
-                        }
+    prediction = prediction.round(4).tolist()
     prediction_result = json.dumps(prediction)
     logger.info(f"prediction_result object: {prediction_result}")
     return prediction_result
